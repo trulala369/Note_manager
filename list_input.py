716bf973d45e5d5449397598bf188171a188ecbb
trulala369 <elenagor31@gmail.com>
2025-01-10 15:06:41 +0300

    Add files via upload

---
+++ list_input.py
@@ -0,0 +1,7 @@
+# Ввод заголовков в заметку
+title1= input('Введите Заголовок1 ' )
+title2= input('Введите Заголовок2 ' )
+title3= input('Введите Заголовок3 ' )
+
+title = [title1, title2, title3 ]
+print(title)
