a25837d8b43cf06d4e479dfbe0006f35c9dc3182
trulala369 <elenagor31@gmail.com>
2025-01-12 00:35:59 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -3,4 +3,4 @@ import locale
 locale.setlocale(locale.LC_ALL, 'ru_RU') # это я срисовала из инета чтобы было по-русски
 
 created_date= datetime.datetime.strptime('05-01-2025', '%d-%m-%Y')
-print('Дата создания заметки:', created_date.strftime('%B'), ',', created_date.strftime('%d'))
+print('Дата создания заметки:', created_date.strftime('%B,%d'))
